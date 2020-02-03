920a2ee3f932ccaf720cbdc735cf2bf6f6d82102
Antoine Louis <ant.louis@protonmail.com>
2020-02-03 09:58:35 +0100

    update

--- Code/bert/create_pretraining_data.py
+++ Code/bert/create_pretraining_data.py
@@ -145,20 +145,20 @@ def write_instance_to_example_files(instances, tokenizer, max_seq_length,
 
     total_written += 1
 
-    if inst_index < 20:
-      tf.logging.info("*** Example ***")
-      tf.logging.info("tokens: %s" % " ".join(
-          [tokenization.printable_text(x) for x in instance.tokens]))
-
-      for feature_name in features.keys():
-        feature = features[feature_name]
-        values = []
-        if feature.int64_list.value:
-          values = feature.int64_list.value
-        elif feature.float_list.value:
-          values = feature.float_list.value
-        tf.logging.info(
-            "%s: %s" % (feature_name, " ".join([str(x) for x in values])))
+    #if inst_index < 20:
+    #  tf.logging.info("*** Example ***")
+    #  tf.logging.info("tokens: %s" % " ".join(
+    #      [tokenization.printable_text(x) for x in instance.tokens]))
+    #
+    #  for feature_name in features.keys():
+    #    feature = features[feature_name]
+    #    values = []
+    #    if feature.int64_list.value:
+    #      values = feature.int64_list.value
+    #    elif feature.float_list.value:
+    #      values = feature.float_list.value
+    #    tf.logging.info(
+    #        "%s: %s" % (feature_name, " ".join([str(x) for x in values])))
 
   for writer in writers:
     writer.close()
