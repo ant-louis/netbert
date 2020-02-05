ddbb32a52e648778e0640c1f22c4a8534e923479
Antoine Louis <ant.louis@protonmail.com>
2020-02-05 15:10:39 +0100

    Update notes

--- Code/bert_singleGPU/run_pretraining.py
+++ Code/bert_singleGPU/run_pretraining.py
@@ -22,7 +22,6 @@ import os
 import modeling
 import optimization
 import tensorflow as tf
-import logging
 
 os.environ['CUDA_VISIBLE_DEVICES']='0' # Make only one GPU visible
 
@@ -180,7 +179,7 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
       train_op = optimization.create_optimizer(
           total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
       
-      logging_hook = tf.train.LoggingTensorHook({"Total loss": total_loss}, every_n_iter=10) 
+      logging_hook = tf.train.LoggingTensorHook({"loss": total_loss}, every_n_iter=10) 
       output_spec = tf.contrib.tpu.TPUEstimatorSpec(
           mode=mode,
           loss=total_loss,
