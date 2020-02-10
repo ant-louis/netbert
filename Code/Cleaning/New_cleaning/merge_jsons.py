06ae791ef9ead91988fa9fedd7a3bd8c0dad9501
Antoine Louis <ant.louis@protonmail.com>
2020-02-10 17:14:16 +0100

    Finish new cleaning scripts

--- Code/Cleaning/New_cleaning/merge_jsons.py
+++ Code/Cleaning/New_cleaning/merge_jsons.py
@@ -3,24 +3,28 @@ import sys
 import json
 import argparse
 
-if __name__ == '__main__':
 
+def parse_arguments():
+    """
+    Parser.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--json_path", type=str, default=".",
         help="path where all the json files are located")
-
     parser.add_argument("--output_file", type=str, default="merged_output.json",
         help="filename where the merged json should go")
+    arguments, _ = parser.parse_known_args()
+    return arguments
 
-    args = parser.parse_args()
 
+def main(args):
+    """
+    """
     json_path = args.json_path
     out_file = args.output_file
-
     json_files = glob.glob(json_path + '/*.json')
 
     counter = 0
-
     with open(out_file, 'w') as outfile:
         for fname in json_files:
             counter += 1
@@ -32,8 +36,10 @@ if __name__ == '__main__':
                 for row in infile:
                     each_row = json.loads(row)
                     outfile.write(row)
-
-
     print("Merged file", out_file, flush=True)
 
 
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main(args)
--- Code/Cleaning/New_cleaning/presplit_sentences_json.py
+++ Code/Cleaning/New_cleaning/presplit_sentences_json.py
@@ -1,6 +1,9 @@
 import sys
 import json
 import argparse
+import time
+from multiprocessing import Process
+
 import nltk
 nltk.download('punkt')
 
@@ -14,7 +17,7 @@ def parse_arguments():
     Parser.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default='/raid/antoloui/Master-thesis/Data/',
+    parser.add_argument("--data_dir", type=str, default='/raid/antoloui/Master-thesis/Data/Cleaned/New_cleaning/',
                         help="Path of the data directory.")
     parser.add_argument("--file_id", type=int,
                         help="Id of the input file.")
@@ -24,11 +27,11 @@ def parse_arguments():
     return arguments
 
 
-def split_sentences(data_dir, file_id):
+def split_sentences(file_id, data_dir):
     """
     """
-    in_filename = data_dir + 'Cleaned/New_cleaning/cleaned_' + str(file_id) + '.json'
-    out_filename = data_dir + 'Cleaned/New_cleaning/split_cleaned_' + str(file_id) + '.json'
+    in_filename = data_dir + 'cleaned_' + str(file_id) + '.json'
+    out_filename = data_dir + 'split_cleaned_' + str(file_id) + '.json'
 
     line_seperator = "\n"
     with open(in_filename, 'r') as ifile:
@@ -55,9 +58,29 @@ def split_sentences(data_dir, file_id):
                 # Write to output file
                 parsed['text'] = line_seperator.join(list_sent)
                 ofile.write(json.dumps(parsed)+'\n')
+    print("Process {} done.".format(file_id))
 
 
+def main(args):
+    """
+    Run processes in parallel if --all=True, otherwise run one process.
+    """
+    if args.all:
+        # Instantiating process with arguments
+        process_list = [Process(target=split_sentences, args=(i, args.data_dir,)) for i in range(1,14)]
+        for i, p in enumerate(process_list):
+            print('Process {} is starting...'.format(i+1))
+            p.start()
+            time.sleep(1)
+        # Complete the processes
+        for p in process_list:
+            p.join()       
+    else:
+        i = args.file_id
+        split_sentences(i, args.data_dir)
+
+        
 
 if __name__ == "__main__":
     args = parse_arguments()
-    split_sentences(args.data_dir, args.file_id)
+    main(args)
