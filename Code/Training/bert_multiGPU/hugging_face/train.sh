8e7a9d543d6e09f5519e354d6d0eda437b139624
Antoine Louis <ant.louis@protonmail.com>
2020-02-13 16:12:15 +0100

    Begin implementation of STS evaluation
    Implemented scripts to encode sentences via bert-as-a-service, as
    well as a similarity search to find most similar sentences in a
    document.

--- Code/Training/bert_multiGPU/hugging_face/train.sh
+++ Code/Training/bert_multiGPU/hugging_face/train.sh
@@ -1,13 +1,13 @@
-export TRAIN_FILE=/raid/antoloui/Master-thesis/Data/Cleaned/train.raw
+export TRAIN_FILE=/raid/antoloui/Master-thesis/Data/Cleaned/dev.raw #train.raw
 export OUT_DIR=./output/base_cased/
 export DEV_FILE=/raid/antoloui/Master-thesis/Data/Cleaned/dev.raw
 
 python run_language_modeling.py \
     --model_type=bert \
-    --model_name_or_path=bert-base-cased \ #bert-base-multilingual-cased
+    --model_name_or_path=bert-base-multilingual-cased \
     --do_train \
     --train_data_file=$TRAIN_FILE \
-    --per_gpu_train_batch_size=14 \
+    --per_gpu_train_batch_size=4 \
     --num_train_epochs 3 \
     --warmup_steps=1000 \
     --save_steps=500 \
@@ -15,8 +15,7 @@ python run_language_modeling.py \
     --mlm \
     --mlm_probability=0.15 \
     --output_dir=$OUT_DIR \
+    --overwrite_output_dir \
     --cache_dir=./cache \
     --do_eval \
     --eval_data_file=$DEV_FILE |& tee ./output/base_cased/training_logs.txt
-
-
