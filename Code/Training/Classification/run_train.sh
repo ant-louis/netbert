e1bb20278b477141d35e29ea251ba42d3eb190c3
Antoine Louis <ant.louis@protonmail.com>
2020-03-03 16:50:45 +0100

    Begin script of word embeddings w. BERT

---
+++ Code/Training/Classification/run_train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export TRAIN_FILE=/raid/antoloui/Master-thesis/Data/Classification/cam_query_to_doctype.csv
+export MODEL=bert-base-cased   #../models/netbert/checkpoint-830000/
+export EPOCHS=4
+export OUT_DIR=./output/finetuned_bertbase/   #./output/finetuned_netbert/
+export GPU=1   #0
+export LOGS=./output/logs/logs_bertbase.txt   #./output/logs/logs_netbert.txt
+
+python -W ignore -u train.py \
+    --filepath $TRAIN_FILE \
+    --model_name_or_path $MODEL \
+    --num_epochs $EPOCHS \
+    --output_dir $OUT_DIR \
+    --gpu_id $GPU |& tee $LOGS
