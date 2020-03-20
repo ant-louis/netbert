ad234c4d6b9274edc664d6fcdbc723949dd0b3e6
Antoine Louis <ant.louis@protonmail.com>
2020-03-20 13:27:43 +0100

    Modify training script of query classification
    Modify script to use all GPUs with DataParallel.

--- Code/Extrinsic_evaluation/Classification/run_train.sh
+++ Code/Extrinsic_evaluation/Classification/run_train.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+export MODEL=/raid/antoloui/Master-thesis/Code/_models/netbert-1027000
 export TRAIN_FILE=/raid/antoloui/Master-thesis/Data/Classification/cam_query_to_doctype.csv
-export MODEL=../_models/netbert/checkpoint-1027000/   #bert-base-cased
 export EPOCHS=4
-export OUT_DIR=./output/finetuned_netbert/   #./output/finetuned_bertbase/
-export GPU=0   #1
-export LOGS=./output/logs/logs_netbert.txt   #./output/logs/logs_bertbase.txt
+export BATCHES=256
+export LOGS=./output/logs/logs_netbert.txt
 
 python -W ignore -u train.py \
-    --filepath $TRAIN_FILE \
     --model_name_or_path $MODEL \
+    --filepath $TRAIN_FILE \
     --num_epochs $EPOCHS \
-    --output_dir $OUT_DIR \
-    --gpu_id $GPU |& tee $LOGS
+    --batch_size $BATCHES \
+    --balanced |& tee $LOGS
