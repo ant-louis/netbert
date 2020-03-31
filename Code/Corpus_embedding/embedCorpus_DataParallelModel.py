a0bb8b717e0a8ae05c0be215f0b757c855b245dc
Antoine Louis <ant.louis@protonmail.com>
2020-03-31 17:33:51 +0200

    Finish modifying embed corpus script: PURE embedding now

--- Code/Corpus_embedding/embedCorpus_DataParallelModel.py
+++ Code/Corpus_embedding/embedCorpus_DataParallelModel.py
@@ -7,15 +7,17 @@ import logging
 import warnings
 warnings.filterwarnings('ignore')
 
+import torch
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-from transformers import BertModel, BertTokenizer
+from transformers import BertTokenizer
+from tokenizers import BertWordPieceTokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-import torch
 import parallel
+from transformers import BertModel
 
 
 def parse_arguments():
@@ -25,16 +27,16 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_dir", 
                         type=str, 
-                        default='/raid/antoloui/Master-thesis/Data/Cleaned/normal_cleaning/split/',
+                        default='/raid/antoloui/Master-thesis/Data/Cleaned/test/',
                         help="Path of the input directory."
                        )
     parser.add_argument("--output_dir", 
                         type=str, 
-                        default='/raid/antoloui/Master-thesis/Data/Embeddings/',
+                        default='/raid/antoloui/Master-thesis/Data/Embeddings/test/',
                         help="Path of the output directory."
                        )
     parser.add_argument("--model_name_or_path",
-                        default='/raid/antoloui/Master-thesis/Code/_models/netbert/checkpoint-1825000/',
+                        default='/raid/antoloui/Master-thesis/Code/_models/netbert-830000/',
                         type=str,
                         #required=True,
                         help="Path to pre-trained model or shortcut name.",
@@ -45,7 +47,7 @@ def parse_arguments():
                         help="Where do you want to store the pre-trained models downloaded from s3.",
     )
     parser.add_argument("--batch_size",
-                        default=1024,
+                        default=768,
                         type=int, 
                         help="Batch size per GPU/CPU."
     )
@@ -80,16 +82,86 @@ def load_sentences(filepath):
     return sentences
 
 
-def sort_sentences(sentences):
+def tokenize(tokenizer, sentences):
     """
-    Given a list of sentences, sort them according to their length.
-    This is done in order for the batches to have approximately the
-    same length, avoiding mixing very short and very long sentences,
-    within the same batch, which results in padding the very short 
-    sentences and thus messing up the embedding when averaging the word
-    embeddings with lots of padding tokens.
+    Given a list of sentences, convert words to vocab ids (0 -> 30522) in each sentence.
+    This function is supposed to use BertTokenizer from the transformers library.
     """
-    return sorted(sentences, key=len)
+    logging.getLogger("transformers.tokenization_utils").setLevel(logging.ERROR)  # No warning on sample size (I deal with that below).
+    indexed_tokens = [tokenizer.encode(sent, add_special_tokens=True) for sent in sentences]    
+    return indexed_tokens
+
+
+def tokenize_fast(tokenizer, sentences):
+    """
+    Given a list of sentences, convert words to vocab ids (0 -> 30522) in each sentence.
+    This function is supposed to use BertWordPieceTokenizer from the tokenizers library.
+    """
+    outputs = [tokenizer.encode(sent) for sent in sentences]
+    indexed_tokens = [out.ids for out in outputs]
+    return indexed_tokens
+
+
+def create_chunks(sentences, indexed_tokens):
+    """
+    Given the tokenized sentences, return chunks of multiple sentences under 512 tokens.
+    """
+    token_chunks = [] 
+    sentence_chunks = []
+
+    # While list of tokenized sentences is not empty...
+    while indexed_tokens:
+
+        # Create the new chunk.
+        chunk_tok = []
+        chunk_sent = []
+
+        # If:
+        #  - The current chunk is empty;
+        #  - There is still sentences to pop in the list;
+        #  - The next sentence to pop is bigger than 512 tokens;
+        if (not chunk_tok) and (indexed_tokens) and (len(indexed_tokens[0]) > 512):
+            # Append the next sentence to the chunk (this chunk will be truncated later).
+            chunk_tok.extend(indexed_tokens.pop(0))
+            chunk_sent.append(sentences.pop(0))
+
+        # While:
+        #   - There is still sentences to pop in the list;
+        #   - The length of the current chunk combined with the length of the next sentence to pop is lower than 512 tokens;
+        while (indexed_tokens) and ((len(chunk_tok) + len(indexed_tokens[0])) <= 512):
+            # Pop the next tokenized sentence and append it to the chunk.
+            chunk_tok.extend(indexed_tokens.pop(0))
+            chunk_sent.append(sentences.pop(0))
+
+        # Concat all sentences in a chunk and remove \n.
+        chunk_sent = ' '.join(chunk_sent).replace('\n', ' ')
+
+        # Add that chunk to my list of chunks.
+        token_chunks.append(chunk_tok)
+        sentence_chunks.append(chunk_sent)
+        
+    return token_chunks, sentence_chunks
+
+
+def pad_and_truncate_chunks(token_chunks):
+    """
+    Given a list of tokenized chunks, pad/truncate them so that they all have the same length.
+    """
+    # Define length of longest tokenized chunk in the batch.
+    lengths = [len(i) for i in token_chunks]
+    max_len = max(lengths) if max(lengths) <= 512 else 512
+
+    # Pad/truncate chunks.
+    padded_chunks = pad_sequences(token_chunks, maxlen=max_len, dtype="long", value=0, truncating="post", padding="post")
+    return padded_chunks
+    
+
+def create_attention_masks(padded_chunks):
+    """
+    Given a list of tokenized padded chunks, create the attention masks for each of them.
+    """
+    attention_masks = np.where(padded_chunks != 0, 1, 0)  #returns ndarray which is 1 if padded_tokens != 0 is True and 0 if False.
+    return attention_masks
 
 
 def gather_sentence_outputs(outputs):
@@ -128,7 +200,7 @@ def gather_word_outputs(outputs):
     return gathered
 
 
-def encode_sentences(sentences, tokenizer, model, device, batch_size):
+def encode_chunks(model, device, sentence_chunks, padded_chunks, attention_masks, batch_size):
     """
     Encoding sentences with CPU/GPU(s).
     
@@ -140,34 +212,22 @@ def encode_sentences(sentences, tokenizer, model, device, batch_size):
     However, once again, the utilisation of the GPUs is very volatile (never at 100% all the time).
     """
     all_embeddings = []
-    iterator = range(0, len(sentences), batch_size)
+    iterator = range(0, len(sentence_chunks), batch_size)
     for batch_idx in tqdm(iterator, desc="   Batches"):
         
-        # Get the batch.
+        # Get the batch indices.
         batch_start = batch_idx
-        batch_end = min(batch_start + batch_size, len(sentences))
-        batch_sentences = sentences[batch_start:batch_end]
+        batch_end = min(batch_start + batch_size, len(sentence_chunks))
         
-        # Tokenize each sentence of the batch.
-        logging.getLogger("transformers.tokenization_utils").setLevel(logging.ERROR)  # No warning on sample size (I deal with that below).
-        tokenized = [tokenizer.encode(sent, add_special_tokens=True) for sent in batch_sentences]
+        # Get the current batch.
+        batch_input_ids = padded_chunks[batch_start:batch_end]
+        batch_attention_masks = attention_masks[batch_start:batch_end]
         
-        # Pad/Truncate sentences to max_len or 512.
-        lengths = [len(i) for i in tokenized]
-        max_len = max(lengths) if max(lengths) <= 512 else 512
-        padded = pad_sequences(tokenized, maxlen=max_len, dtype="long", 
-                          value=0, truncating="post", padding="post")
-        
-        # Create attention masks.
-        attention_mask = np.where(padded != 0, 1, 0)  #returns ndarray which is 1 if padded != 0 is True and 0 if False.
-        
-        # Convert inputs to torch tensors.
-        input_ids = torch.tensor(padded)
-        attention_mask = torch.tensor(attention_mask)
-        
-        # Push inputs to GPUs.
-        input_ids = input_ids.to(device)
-        attention_mask = attention_mask.to(device)
+        # Convert model inputs to torch tensors and push them to GPUs.
+        batch_input_ids = torch.tensor(batch_input_ids)
+        batch_input_ids = batch_input_ids.to(device)
+        batch_attention_masks = torch.tensor(batch_attention_masks)
+        batch_attention_masks = batch_attention_masks.to(device)
         
         # Encode batch.
         model.eval()
@@ -176,32 +236,40 @@ def encode_sentences(sentences, tokenizer, model, device, batch_size):
             #  - output[0] is the last_hidden_state, i.e a tensor of shape (batch_size, sequence_length, hidden_size).
             #  - output[1] is the pooler_output, i.e. a tensor of shape (batch_size, hidden_size) being the last layer hidden-state of the first token of the sequence (classification token).
             #  - output[2] are all hidden_states, i.e. a 13-tuple of torch tensors of shape (batch_size, sequence_length, hidden_size): 12 encoders-outputs + initial embedding outputs.
-            outputs = model(input_ids, attention_mask=attention_mask)
+            outputs = model(batch_input_ids, attention_mask=batch_attention_masks)
             
         # Gather outputs from the different GPUs.
         last_hidden_states = gather_sentence_outputs(outputs)
 
         # For each sentence, take the embeddings of its word from the last layer and represent that sentence by their average.
-        sentence_embeddings = [torch.mean(embeddings[:torch.squeeze((masks == 1).nonzero(), dim=1).shape[0]], dim=0).to('cpu').numpy() for embeddings, masks in zip(last_hidden_states, attention_mask)]
-        all_embeddings.extend(sentence_embeddings)
+        chunk_embeddings = [torch.mean(embeddings[:torch.squeeze((masks == 1).nonzero(), dim=1).shape[0]], dim=0).to('cpu').numpy() for embeddings, masks in zip(last_hidden_states, batch_attention_masks)]
+        all_embeddings.extend(chunk_embeddings)
         
     # Create dataframe for storing embeddings.
     all_embeddings = np.array(all_embeddings)
     cols = ['feat'+str(i+1) for i in range(all_embeddings.shape[1])]
     df = pd.DataFrame(data=all_embeddings[:,:], columns=cols)
-    df['Sentence'] = sentences
+    df['Chunk'] = sentence_chunks
+    
     return df
 
 
+
 def main(args):
-    """
-    """
     print("\n===================================================")
     print("Loading pretrained model/tokenizer...")
     print("===================================================\n")
     t0 = time.time()
     tokenizer = BertTokenizer.from_pretrained(args.model_name_or_path)
-    model = BertModel.from_pretrained(args.model_name_or_path, output_hidden_states=True, cache_dir=args.cache_dir) # Will output all hidden_states.
+    fast_tokenizer = BertWordPieceTokenizer(args.model_name_or_path + 'vocab.txt',
+                                            add_special_tokens=True,
+                                            lowercase=False,
+                                            clean_text=True, 
+                                            handle_chinese_chars=True,
+                                            strip_accents=True)
+    model = BertModel.from_pretrained(args.model_name_or_path, 
+                                      output_hidden_states=True, # Will output all hidden_states.
+                                      cache_dir=args.cache_dir)
     print("   Loaded checkpoint '{}'.  -  Took: {}".format(args.model_name_or_path, format_time(time.time() - t0)))
     
     print("\n===================================================")
@@ -222,15 +290,34 @@ def main(args):
         print("   Loading sentences from {}...".format(file))
         t0 = time.time()
         sentences = load_sentences(file)
-        sorted_sentences = sort_sentences(sentences)
-        sorted_sentences = sorted_sentences[::-1]
-        print("   {} sentences loaded.  -  Took: {}".format(len(sentences), format_time(time.time() - t0)))
-
-        print("   Encoding sentences...")
+        print("     - {} sentences loaded.  -  Took: {}".format(len(sentences), format_time(time.time() - t0)))
+        
+        print("   Tokenizing sentences...")
         t0 = time.time()
-        df = encode_sentences(sorted_sentences, tokenizer, model, device, args.batch_size)
+        #indexed_tokens = tokenize(tokenizer, sentences)
+        indexed_tokens = tokenize_fast(fast_tokenizer, sentences)
+        print("     - {} sentences tokenized.  -  Took: {}".format(len(indexed_tokens), format_time(time.time() - t0)))
+        
+        print("   Creating chunks of max 512 tokens...")
+        t0 = time.time()
+        token_chunks, sentence_chunks = create_chunks(sentences, indexed_tokens)
+        print("     - {} chunks created.  -  Took: {}".format(len(token_chunks), format_time(time.time() - t0)))
+        
+        print("   Padding/truncating the chunks...")
+        t0 = time.time()
+        padded_chunks = pad_and_truncate_chunks(token_chunks)
+        print("     - {} chunks padded/truncated.  -  Took: {}".format(len(padded_chunks), format_time(time.time() - t0)))
+        
+        print("   Creating attention masks...")
+        t0 = time.time()
+        attention_masks = create_attention_masks(padded_chunks)
+        print("     - {} attention masks created.  -  Took: {}".format(len(attention_masks), format_time(time.time() - t0)))
+
+        print("   Encoding chunks...")
+        t0 = time.time()
+        df = encode_chunks(model, device, sentence_chunks, padded_chunks, attention_masks, args.batch_size)
         elapsed = time.time() - t0
-        print("   {} sentences embedded. -  Took: {:}  ({:.2f} s/sentences)".format(len(sorted_sentences), format_time(elapsed), elapsed/len(sorted_sentences)))
+        print("     - {} chunks encoded. -  Took: {:}  ({:.2f} s/sentences)".format(len(padded_chunks), format_time(elapsed), elapsed/len(sentence_chunks)))
 
         print("   Saving dataframe to {}...".format(args.output_dir))
         t0 = time.time()
@@ -238,7 +325,7 @@ def main(args):
         output_path = args.output_dir + filename + '.h5'
         df.to_hdf(output_path, key='df', mode='w')
         #df.to_csv(output_path, sep=',', encoding='utf-8', float_format='%.10f', decimal='.', index=False)
-        print("   Dataframe saved. -  Took: {}\n".format(format_time(time.time() - t0)))
+        print("     - Dataframe saved. -  Took: {}\n".format(format_time(time.time() - t0)))
     
 
 if __name__=="__main__":
