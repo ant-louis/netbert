e9f724e1c7ac051e1380bd992453671f9c74ca9c
Antoine Louis <ant.louis@protonmail.com>
2020-04-01 11:21:18 +0200

    Finish embeddings merging script

--- Code/Corpus_embedding/embed.sh
+++ Code/Corpus_embedding/embed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export IN_DIR=/raid/antoloui/Master-thesis/Data/Cleaned/split/
+export IN_DIR=/raid/antoloui/Master-thesis/Data/Cleaned/
 export OUT_DIR=/raid/antoloui/Master-thesis/Data/Embeddings/
 export CACHE=/raid/antoloui/Master-thesis/Code/_cache/
 
@@ -8,7 +8,7 @@ export MODEL=/raid/antoloui/Master-thesis/Code/_models/netbert-830000/
 export BATCH_SIZE=1408
 
 
-python -W ignore -u embed_corpus.py \
+python -W ignore -u tools/embed_corpus.py \
        --input_dir=$IN_DIR \
        --output_dir=$OUT_DIR \
        --cache_dir=$CACHE \
