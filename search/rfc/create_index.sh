db0bb478084d8169ed7e2256689f83f14d79054f
Antoine Louis <ant.louis@protonmail.com>
2020-04-17 11:17:55 +0200

    Finish implementation of rfc search

---
+++ search/rfc/create_index.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+INDIR=/raid/antoloui/Master-thesis/_data/search/rfc/embeddings/
+OUTDIR=/raid/antoloui/Master-thesis/_data/search/rfc/index/
+N_GPU=8
+METHOD=cos
+
+
+python -W ignore -u tools/create_faiss_index.py \
+       --input_dir $INDIR \
+       --output_dir $OUTDIR \
+       --n_gpu $N_GPU \
+       --method $METHOD
