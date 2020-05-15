c27a069982aa83b24110d56df088a4591ce98912
Antoine Louis <ant.louis@protonmail.com>
2020-05-15 19:38:34 +0200

    Correct finetuning netbert sentence classification

--- extrinsic_evaluation/classification/train.sh
+++ extrinsic_evaluation/classification/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=5
 
 #export MODEL=bert_base_cased
 export MODEL=netbert-final
@@ -9,16 +9,16 @@ export MODELS_PATH=/raid/antoloui/Master-thesis/_models/
 export TRAIN_FILE=/raid/antoloui/Master-thesis/_data/classification/cam_query_to_doctype.csv
 export LABELS=5
 
-export EPOCHS=4
-export BATCHES=16  #16, 32
-export LR=5e-5  #5e-5, 3e-5, 2e-5
+export EPOCHS=6
+export BATCHES=32  #16, 32
+export LR=2e-5  #5e-5, 3e-5, 2e-5
 
 python -W ignore -u train.py \
     --model_name_or_path $MODELS_PATH/$MODEL \
     --do_train \
+    --do_val \
     --training_filepath $TRAIN_FILE \
     --num_labels $LABELS \
     --num_epochs $EPOCHS \
     --batch_size $BATCHES \
-    --learning_rate $LR \
-    --do_eval  #|& tee ./output/$MODEL/training_logs.txt
+    --learning_rate $LR
