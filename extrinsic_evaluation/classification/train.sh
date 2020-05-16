28a3517b4efb535a55840d13e60505b6ddc9b49a
Antoine Louis <ant.louis@protonmail.com>
2020-05-16 21:47:05 +0200

    Update new tests text classification

--- extrinsic_evaluation/classification/train.sh
+++ extrinsic_evaluation/classification/train.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=0
 
-#export MODEL=bert_base_cased
-export MODEL=netbert-final
+export MODEL=bert_base_cased
+#export MODEL=netbert-final
 
 export MODELS_PATH=/raid/antoloui/Master-thesis/_models/
 export TRAIN_FILE=/raid/antoloui/Master-thesis/_data/classification/cam_query_to_doctype.csv
 export LABELS=5
 
 export EPOCHS=6
-export BATCHES=32  #16, 32
-export LR=2e-5  #5e-5, 3e-5, 2e-5
+export BATCHES=16  #16, 32
+export LR=3e-5  #5e-5, 3e-5, 2e-5
 
 python -W ignore -u train.py \
     --model_name_or_path $MODELS_PATH/$MODEL \
     --do_train \
-    --do_val \
+    --do_test \
     --training_filepath $TRAIN_FILE \
     --num_labels $LABELS \
     --num_epochs $EPOCHS \
