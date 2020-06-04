9a43b9efc91e300fb0780f0cf66f0f830fcfe7ac
Antoine Louis <ant.louis@protonmail.com>
2020-06-04 13:25:34 +0200

    Update training script text classification

--- experiments/text_classification/test.sh
+++ experiments/text_classification/test.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 #export MODEL=bert_base_cased
 export MODEL=netbert-final
 
-export MODELS_PATH=/raid/antoloui/Master-thesis/_models/
+export MODELS_PATH=/raid/antoloui/Master-thesis/experiments/text_classification/output
 export FILE=/raid/antoloui/Master-thesis/_data/classification/cam_query_to_doctype.csv
 export LABELS=5
-
-export EPOCHS=6
-export BATCHES=16  #16, 32
-export LR=3e-5  #5e-5, 3e-5, 2e-5
+export BS=768
 
 python -W ignore -u train.py \
     --model_name_or_path $MODELS_PATH/$MODEL \
-    --do_train \
-    --do_val \
+    --do_test \
     --filepath $FILE \
     --num_labels $LABELS \
-    --num_epochs $EPOCHS \
-    --batch_size $BATCHES \
-    --learning_rate $LR
+    --batch_size $BS \
+#    --do_compare
